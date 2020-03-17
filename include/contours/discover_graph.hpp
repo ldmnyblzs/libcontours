af0ae00f43d474ce8c8e5a79c660523dcf4ea861
Balázs Ludmány <ludmany.balazs@cloud.bme.hu>
2020-03-17 11:27:13 +0100

    Add license

--- include/contours/discover_graph.hpp
+++ include/contours/discover_graph.hpp
@@ -1,3 +1,22 @@
+/*
+  Copyright 2019 Balázs Ludmány
+
+  This file is part of libcontours.
+
+  libcontours is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  libcontours is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with libcontours.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef DISCOVER_GRAPH_HPP
 #define DISCOVER_GRAPH_HPP 1
 
